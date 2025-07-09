d045fd51af24fabc557192e9f3fd0128f9c4fb1f
owen nguyen <owennguyen1@gmail.com>
2025-07-09 16:44:58 +0700

    chore: enhance Tailwind CSS configuration with new color variables and update UI components

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -22,21 +22,41 @@ RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
 
 const RadioGroupItem = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
->(({ className, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item> & {
+    title?: string;
+  }
+>(({ className, title, ...props }, ref) => {
+  const baseClasses = "aspect-square h-5 w-5 rounded-full border-2 focus:outline-none border-red-600 bg-red-50 hover:ring-2 hover:ring-red-100 hover:bg-radio-default";
+
+  const checkedClasses = "data-[state=checked]:bg-radio-default data-[state=checked]:border-red-600 data-[state=checked]:border-[1px]";
+
+  const disabledClasses = `disabled:bg-grey-100 disabled:border-grey-200 disabled:border-[1px] disabled:pointer-events-none disabled:cursor-not-allowed
+                           disabled:data-[state=checked]:bg-grey-100 disabled:data-[state=checked]:border-grey-200 disabled:data-[state=checked]:border-[1px]`;
+
   return (
-    <RadioGroupPrimitive.Item
-      ref={ref}
-      className={cn(
-        "aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-        className
-      )}
-      {...props}
-    >
-      <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <Circle className="h-2.5 w-2.5 fill-current text-current" />
-      </RadioGroupPrimitive.Indicator>
-    </RadioGroupPrimitive.Item>
+    <div className="flex items-center gap-2">
+      <RadioGroupPrimitive.Item
+        ref={ref}
+        className={cn(
+          baseClasses,
+          checkedClasses,
+          disabledClasses,
+          className
+        )}
+        {...props}
+      >
+        <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
+          <Circle className={`h-3 w-3 ${props.disabled ? "fill-grey-200 text-grey-200" : "fill-red-600 text-red-600"}`} />
+        </RadioGroupPrimitive.Indicator>
+
+      </RadioGroupPrimitive.Item>
+      <label
+        className={`text-body-lg text-black hover:cursor-pointer hover:text-red-600 ${props.disabled ? "text-grey-200" : ""}`}
+        htmlFor={props.id}
+      >
+        {title}
+      </label>
+    </div>
   );
 });
 RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName;
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,160 +1,83 @@
 "use client";
 
+import { cva, VariantProps } from "class-variance-authority";
+import { CheckCircleIcon, ChevronDown, Info } from "lucide-react";
 import * as React from "react";
-import * as SelectPrimitive from "@radix-ui/react-select";
-import { Check, ChevronDown, ChevronUp } from "lucide-react";
 
 import { cn } from "~/lib/utils";
 
-const Select = SelectPrimitive.Root;
+const selectVariants = cva(
+  "flex min-h-12 w-full bg-white py-2 pl-4 pr-20 gap-2 text-body-lg placeholder:text-grey-300 focus:outline-none focus:text-grey-800 cursor-pointer",
+  {
+    variants: {
+      variant: {
+        default: "border border-grey-200 hover:border-grey-500 focus:border-grey-600 disabled:bg-grey-700 disabled:text-black disabled:placeholder:text-black disabled:opacity-15 disabled:pointer-events-none",
+        warning: "border border-warning",
+        success: "border border-success",
+      },
+      typeStyle: {
+        pill: "rounded-full",
+        round: "rounded",
+      }
+    },
+    defaultVariants: {
+      variant: "default",
+      typeStyle: "round",
+    },
+  }
+);
 
-const SelectGroup = SelectPrimitive.Group;
+export interface SelectProps
+  extends React.SelectHTMLAttributes<HTMLSelectElement>,
+  VariantProps<typeof selectVariants> {
+  asChild?: boolean;
+  label?: string;
+  helperText?: string;
+  leadingIcon?: React.ReactNode;
+}
 
-const SelectValue = SelectPrimitive.Value;
+const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
+  ({ className, label, variant, typeStyle, helperText, leadingIcon, ...props }, ref) => {
 
-const SelectTrigger = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-  <SelectPrimitive.Trigger
-    ref={ref}
-    className={cn(
-      "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[placeholder]:text-muted-foreground [&>span]:line-clamp-1",
-      className
-    )}
-    {...props}
-  >
-    {children}
-    <SelectPrimitive.Icon asChild>
-      <ChevronDown className="h-4 w-4 opacity-50" />
-    </SelectPrimitive.Icon>
-  </SelectPrimitive.Trigger>
-));
-SelectTrigger.displayName = SelectPrimitive.Trigger.displayName;
+    const getTrailingIcon = () => {
+      if (variant === "warning") return <Info className="w-5 h-5 text-warning" />;
+      if (variant === "success") return <CheckCircleIcon className="w-5 h-5 text-success" />;
+      return null;
+    };
 
-const SelectScrollUpButton = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.ScrollUpButton>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollUpButton>
->(({ className, ...props }, ref) => (
-  <SelectPrimitive.ScrollUpButton
-    ref={ref}
-    className={cn(
-      "flex cursor-default items-center justify-center py-1",
-      className
-    )}
-    {...props}
-  >
-    <ChevronUp className="h-4 w-4" />
-  </SelectPrimitive.ScrollUpButton>
-));
-SelectScrollUpButton.displayName = SelectPrimitive.ScrollUpButton.displayName;
+    const getHelperTextColor = () => {
+      if (variant === "warning") return "text-warning";
+      if (variant === "success") return "text-success text-body-sm";
+      if (props.disabled) return "text-grey-400";
+      return "text-grey-600";
+    };
 
-const SelectScrollDownButton = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.ScrollDownButton>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollDownButton>
->(({ className, ...props }, ref) => (
-  <SelectPrimitive.ScrollDownButton
-    ref={ref}
-    className={cn(
-      "flex cursor-default items-center justify-center py-1",
-      className
-    )}
-    {...props}
-  >
-    <ChevronDown className="h-4 w-4" />
-  </SelectPrimitive.ScrollDownButton>
-));
-SelectScrollDownButton.displayName =
-  SelectPrimitive.ScrollDownButton.displayName;
+    return (
+      <div className="flex flex-col gap-1 items-start">
+        <label htmlFor={props.id} className={`text-title-md text-black font-bold ${props.disabled ? "opacity-15 pointer-events-none" : ""}`}>{label}</label>
+        <div className="relative">
+          {leadingIcon && <span className="absolute inset-y-0 left-2 flex gap-2 items-center">{leadingIcon}</span>}
+          <select
+            className={cn(
+              selectVariants({ variant, typeStyle, className }),
+              leadingIcon ? "pl-10" : "",
+              className
+            )}
+            ref={ref}
+            {...props}
+          />
 
-const SelectContent = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, position = "popper", ...props }, ref) => (
-  <SelectPrimitive.Portal>
-    <SelectPrimitive.Content
-      ref={ref}
-      className={cn(
-        "relative z-50 max-h-[--radix-select-content-available-height] min-w-[8rem] origin-[--radix-select-content-transform-origin] overflow-y-auto overflow-x-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-        position === "popper" &&
-          "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
-        className
-      )}
-      position={position}
-      {...props}
-    >
-      <SelectScrollUpButton />
-      <SelectPrimitive.Viewport
-        className={cn(
-          "p-1",
-          position === "popper" &&
-            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
-        )}
-      >
-        {children}
-      </SelectPrimitive.Viewport>
-      <SelectScrollDownButton />
-    </SelectPrimitive.Content>
-  </SelectPrimitive.Portal>
-));
-SelectContent.displayName = SelectPrimitive.Content.displayName;
+          <span className={`absolute inset-y-0 right-4 flex gap-2 items-center pointer-events-none ${props.disabled ? "opacity-15" : ""}`}>
+            {getTrailingIcon()}
+            <ChevronDown className="w-5 h-5 text-grey-700" />
+          </span>
 
-const SelectLabel = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Label>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Label>
->(({ className, ...props }, ref) => (
-  <SelectPrimitive.Label
-    ref={ref}
-    className={cn("py-1.5 pl-8 pr-2 text-sm font-semibold", className)}
-    {...props}
-  />
-));
-SelectLabel.displayName = SelectPrimitive.Label.displayName;
+        </div>
+        {helperText && <p className={`text-body-md ${getHelperTextColor()}`}>{helperText}</p>}
+      </div>
+    );
+  }
+);
+Select.displayName = "Select";
 
-const SelectItem = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
->(({ className, children, ...props }, ref) => (
-  <SelectPrimitive.Item
-    ref={ref}
-    className={cn(
-      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-      className
-    )}
-    {...props}
-  >
-    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-      <SelectPrimitive.ItemIndicator>
-        <Check className="h-4 w-4" />
-      </SelectPrimitive.ItemIndicator>
-    </span>
-
-    <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
-  </SelectPrimitive.Item>
-));
-SelectItem.displayName = SelectPrimitive.Item.displayName;
-
-const SelectSeparator = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Separator>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Separator>
->(({ className, ...props }, ref) => (
-  <SelectPrimitive.Separator
-    ref={ref}
-    className={cn("-mx-1 my-1 h-px bg-muted", className)}
-    {...props}
-  />
-));
-SelectSeparator.displayName = SelectPrimitive.Separator.displayName;
-
-export {
-  Select,
-  SelectGroup,
-  SelectValue,
-  SelectTrigger,
-  SelectContent,
-  SelectLabel,
-  SelectItem,
-  SelectSeparator,
-  SelectScrollUpButton,
-  SelectScrollDownButton,
-};
+export { Select };
