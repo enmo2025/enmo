f7cb0136da06d4e35c2ec695b93e0de3a273e9d1
Owen Nguyen <owen.nguyen.thatera@gmail.com>
2025-07-14 13:57:15 +0700

    feat(sidenav): add brown-100 color variable and adjust sidenav layout

--- src/components/layout/header/sidenav.tsx
+++ src/components/layout/header/sidenav.tsx
@@ -31,38 +31,40 @@ export default function Sidenav({ title, listNav, className }: SidenavProps) {
   };
 
   return (
-    <div
-      className={cn(
-        `fixed bottom-0 bg-brown-100 top-[${HEADER_HEIGHT}px] h-[calc(100vh-${HEADER_HEIGHT}px)] w-[20vw] overflow-y-auto p-8`,
-        className
-      )}
-    >
-      <h1 className="text-center text-display-sm font-bold text-brown-900">{title}</h1>
-      <div className="flex flex-col gap-4 py-20">
-        {listNav.map((item) => (
-          <div key={item.name}>
-            <Button
-              onClick={() => navigate.push(item.href)}
-              className={cn(
-                'w-full bg-white text-title-lg font-bold text-brown-900',
-                isActive(item.href) && 'bg-red-900 text-white'
-              )}
-              leadingIcon={<item.icon color={getIconColor(item.href)} />}
-              size="xl"
-              typeStyle="round"
-            >
-              {item.name}
-            </Button>
-          </div>
-        ))}
+    <div className="mr-[20vw]">
+      <div
+        className={cn(
+          `fixed bottom-0 bg-brown-100 top-[${HEADER_HEIGHT}px] h-[calc(100vh-${HEADER_HEIGHT}px)] w-[20vw] overflow-y-auto p-8`,
+          className
+        )}
+      >
+        <h1 className="text-center text-display-sm font-bold text-brown-900">{title}</h1>
+        <div className="flex flex-col gap-4 py-20">
+          {listNav.map((item) => (
+            <div key={item.name}>
+              <Button
+                onClick={() => navigate.push(item.href)}
+                className={cn(
+                  'w-full bg-white text-title-lg font-bold text-brown-900',
+                  isActive(item.href) && 'bg-red-900 text-white'
+                )}
+                leadingIcon={<item.icon color={getIconColor(item.href)} />}
+                size="xl"
+                typeStyle="round"
+              >
+                {item.name}
+              </Button>
+            </div>
+          ))}
+        </div>
+        <div className="mb-4 flex w-full items-center justify-center gap-2">
+          <span>
+            <Icons.informationCircle />
+          </span>
+          <span className="text-body-lg text-brown-700">利用規約</span>
+        </div>
+        <LogoutButton />
       </div>
-      <div className="flex w-full items-center justify-center gap-2">
-        <span>
-          <Icons.informationCircle />
-        </span>
-        <span className="text-body-lg text-brown-700">利用規約</span>
-      </div>
-      <LogoutButton />
     </div>
   );
 }
