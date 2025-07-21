30efe248a70934474d2a12d6f0c48e7627845e1f
owen nguyen <owennguyen1@gmail.com>
2025-07-21 13:39:05 +0700

    refactor(admin): standardize import paths for admin components

--- src/components/pages/admin/create-event/index.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import EventForm from '~/components/pages/admin/event-form';
-
-export default function CreateEvent() {
-  return <EventForm />;
-}
--- src/components/pages/admin/create-partner/index.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import PartnerForm from '~/components/pages/admin/partner-form';
-
-export default function CreatePartner() {
-  return <PartnerForm />;
-}
