57eb45ea24bd92b7d2b6d115163cde7a6d7be5d3
owen nguyen <owennguyen1@gmail.com>
2025-07-21 13:01:44 +0700

    feat(auth): add Register layout and update BasicInfo page structure

--- src/app/(modules)/(auth)/register/page.tsx
+++ src/app/(modules)/(auth)/register/page.tsx
@@ -1,13 +1,5 @@
-import { Metadata } from 'next';
 import React from 'react';
 import AuthPanel from '~/components/shared/auth-panel';
-
-export const metadata: Metadata = {
-  title: '登録',
-  description: '登録ページ',
-  manifest: '/manifest.json',
-  keywords: ['登録', 'Register', 'Enmo'],
-};
 
 export default function page() {
   return <AuthPanel type="register" />;
