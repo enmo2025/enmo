1f35d1c151fa497d62a7e5ee332473dc9fcf5580
owen nguyen <owennguyen1@gmail.com>
2025-07-21 14:19:18 +0700

    refactor(event): update EventCard and EventList to use EventDetail interface for partner information

--- src/components/shared/event-list.tsx
+++ src/components/shared/event-list.tsx
@@ -1,12 +1,7 @@
+import { EventDetail } from '~/services/clientService/event/event.interface';
 import EventCard from './event-card';
-import { EventCardProps } from './event-card';
-import { Event } from '@prisma/client';
 
-export interface EventListProps {
-  eventList: EventCardProps[];
-}
-
-export default function EventList({ eventList = [] }: { eventList: Event[] }) {
+export default function EventList({ eventList = [] }: { eventList: EventDetail[] }) {
   return (
     <div className="flex flex-col">
       <div className="grid grid-cols-2 gap-3 md:grid-cols-3 md:gap-5 lg:grid-cols-4">
