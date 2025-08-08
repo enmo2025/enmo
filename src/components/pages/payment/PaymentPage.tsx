41d516eb66c535214ffe9f13fc803aa55c0ade18
mason-pham <mason.pham.thatera@gmail.com>
2025-08-08 14:38:40 +0700

    fix: fix UI of the Payment page

--- src/components/pages/payment/PaymentPage.tsx
+++ src/components/pages/payment/PaymentPage.tsx
@@ -7,7 +7,7 @@ import Divider from '~/components/ui/divider';
 import { Event } from '@prisma/client';
 import { Button } from '~/components/ui/button';
 import NoDataPlaceholder from '~/components/shared/indicator/no-data-placeholder';
-import { formatDate, formatNumber } from '~/lib/utils';
+import { cn, formatDate, formatNumber } from '~/lib/utils';
 import { useRouter } from 'next/navigation';
 import { createCheckoutSession } from '~/services/clientService/stripe/stripe.api';
 
@@ -51,15 +51,15 @@ export default function PaymentPage({ userId, event }: { userId: string; event: 
   };
 
   return (
-    <div className="mx-auto mb-30 mt-14 max-w-screen-md px-4 text-center">
-      <h1 className="mb-10 text-headline-lg font-bold text-red-700">お支払い方法を選択してください</h1>
+    <div className="mx-auto mb-30 mt-14 flex max-w-200 flex-col gap-20 px-4 text-center">
+      <h1 className="text-headline-lg font-bold text-red-700">お支払い方法を選択してください</h1>
       <Card className="bg-white">
         <CardContent>
           <EventSummary event={event} />
           <PriceDetails participantFee={event.participantFee} totalAmount={totalAmount} />
         </CardContent>
       </Card>
-      <Button className="mt-10 w-full" onClick={handlePayment} size="xl" typeStyle="round">
+      <Button className="h-20 w-full text-headline-md" onClick={handlePayment} size="xl" typeStyle="round">
         クレジットカードで支払う
       </Button>
     </div>
@@ -72,12 +72,12 @@ function EventSummary({ event }: EventSummaryProps) {
       <Image
         width={1000}
         height={1000}
-        className="h-[180px] w-full rounded-sm object-cover md:w-[260px]"
+        className="h-[180px] w-full rounded object-cover md:w-[260px]"
         src={event.eventBanner}
         alt="event banner"
       />
       <div className="w-full text-left text-brown-900">
-        <div className="text-headline-sm font-bold text-brown-700">{event.title}</div>
+        <div className="text-headline-sm font-bold text-brown-900">{event.title}</div>
         <div className="mt-2 text-body-md">{event.location}</div>
         <Divider />
         <div className="mt-3 w-full space-y-2 text-body-sm">
@@ -92,9 +92,9 @@ function EventSummary({ event }: EventSummaryProps) {
 
 function EventDetail({ label, value }: EventDetailProps) {
   return (
-    <div className="text-body-sm">
+    <div className="flex items-center gap-5 text-body-sm">
       <span className="text-brown-700">{label}:</span>
-      <span className="ml-3 text-body-lg font-medium">{value}</span>
+      <span className="text-body-lg">{value}</span>
     </div>
   );
 }
@@ -103,14 +103,14 @@ function PriceDetails({ participantFee, totalAmount }: PriceDetailsProps) {
   return (
     <div className="text-left">
       <h1 className="mt-5 text-headline-sm font-bold text-brown-900">価格詳細</h1>
-      <Divider />
+      <Divider className="bg-black" />
       <div>
         <PriceRow label="参加費" amount={participantFee} />
       </div>
-      <Divider />
+      <Divider className="bg-black" />
       <PriceRow label="合計料金" amount={totalAmount} isTotal />
-      <Divider />
-      <p className="text-center text-body-sm text-brown-700">
+      <Divider className="bg-black" />
+      <p className="text-center text-body-md text-brown-700">
         ※表示価格は現在の為替レートに基づく概算価格です。為替レートは変動する場合がありますのでご了承ください。
       </p>
     </div>
@@ -118,13 +118,13 @@ function PriceDetails({ participantFee, totalAmount }: PriceDetailsProps) {
 }
 
 function PriceRow({ label, amount, isTotal = false }: PriceRowProps) {
-  const labelClassName = isTotal ? 'text-body-lg text-brown-700' : 'text-brown-700';
-  const amountClassName = isTotal ? 'ml-3 text-body-lg text-headline-lg font-medium' : 'ml-3 text-body-lg font-medium';
+  const labelClassName = isTotal ? 'text-body-xl text-brown-900' : 'text-body-lg text-brown-700';
+  const amountClassName = cn('ml-3 font-bold text-brown-900', isTotal ? 'text-headline-lg' : 'text-title-lg');
 
   return (
-    <div className="flex justify-between">
+    <div className="flex items-center justify-between">
       <span className={labelClassName}>{label}:</span>
-      <span className={amountClassName}>{formatNumber(amount)}¥</span>
+      <span className={amountClassName}>¥{formatNumber(amount)}</span>
     </div>
   );
 }
